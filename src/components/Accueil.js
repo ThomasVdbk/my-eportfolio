395b7b885377d4bb9915677796aaa9b12cdb7e11
ThomasVdbk <thomas.vdbk@gmail.com>
2024-01-03 15:39:32 +0100

    ajout datas et filter sur les components selon category

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,9 +1,20 @@
 import React from "react";
+import articlesData from "../datas/datas";
 
 function Accueil() {
+  const accueilArticles = articlesData.filter(
+    (article) => article.category === "Accueil"
+  );
+
   return (
     <div>
       <h2>Accueil Component</h2>
+      {accueilArticles.map((article) => (
+        <div key={article.id}>
+          <h3>{article.title}</h3>
+          <p>{article.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
