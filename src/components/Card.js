7f73e6e580bdaa2d2f6cc63a87193ef6b2f4a14d
ThomasVdbk <thomas.vdbk@gmail.com>
2024-01-05 15:49:44 +0100

    ajout css en fonction de expanded

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import "../styles/cardStyle.css";
 
 function Card({ title, content, isSingle }) {
   const truncatedContent = isSingle ? content : content.slice(0, 100) + "...";
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className={`article-card ${isSingle ? "single-article" : ""}`}>
-      <h3>{title}</h3>
-      <p>{truncatedContent}</p>
+    <div
+      className={`article-card ${isSingle ? "single-article" : ""} ${
+        expanded ? "expanded" : ""
+      }`}
+    >
+      <h3 className="header">{title}</h3>
+      <p>{expanded ? content : truncatedContent}</p>
       {isSingle || (
         <button onClick={() => setExpanded(!expanded)}>
           {expanded ? "Réduire" : "Étendre"}
