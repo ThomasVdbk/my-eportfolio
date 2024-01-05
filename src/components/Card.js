32c031c93c985100b5d1a255b03cb053d5647d5d
ThomasVdbk <thomas.vdbk@gmail.com>
2024-01-05 16:34:45 +0100

    sup button expended remplace par handleCardClick

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/cardStyle.css";
 
 function Card({ title, content, isSingle }) {
   const truncatedContent = isSingle ? content : content.slice(0, 100) + "...";
   const [expanded, setExpanded] = useState(false);
+  const cardRef = useRef(null);
+
+  const handleCardClick = () => {
+    if (!isSingle) {
+      setExpanded(!expanded);
+      if (!expanded) {
+        cardRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }
+  };
 
   return (
     <div
+      ref={cardRef}
       className={`article-card ${isSingle ? "single-article" : ""} ${
         expanded ? "expanded" : ""
       }`}
+      onClick={handleCardClick}
     >
       <h3 className="header">{title}</h3>
-      <p>{expanded ? content : truncatedContent}</p>
-      {isSingle || (
-        <button onClick={() => setExpanded(!expanded)}>
-          {expanded ? "Réduire" : "Étendre"}
-        </button>
-      )}
+      <p className={expanded ? "expanded-content" : "truncated-content"}>
+        {expanded ? content : truncatedContent}
+      </p>
     </div>
   );
 }
